df861857c18cb2c4960245b99603d7d50f0d4c3c
B.alirezaei <b.alirezaei@gmail.com>
2022-10-24 11:43:13 +0330

    init

---
+++ RabbitMq_Consumer/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    var queueName = "learnRabbitOct2022";
+    channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (ch, ea) =>
+    {
+        var message = Encoding.UTF8.GetString(ea.Body.Span);
+        channel.BasicAck(ea.DeliveryTag, false);
+        Console.WriteLine(message);
+    };
+    String consumerTag = channel.BasicConsume(queueName, false, consumer);
+    Console.WriteLine("Press Enter to exit");
+    Thread.Sleep(100000);
+}
+
---
+++ RabbitMq_Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest" };
+
+using (var connection = factory.CreateConnection())
+using (var chanel = connection.CreateModel())
+{
+    chanel.QueueDeclare(queue: "learnRabbitOct2022", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+    for (int i = 0; i < 5; i++)
+    {
+        string message = $"This is a message.{DateTime.Now.ToString("D")} - {DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}";
+
+        var body = Encoding.UTF8.GetBytes(message);
+        chanel.BasicPublish(exchange: "", routingKey: "learnRabbitOct2022", basicProperties: null, body: body);
+
+        Console.WriteLine("Sent {0}", message);
+        Thread.Sleep(12000);
+    }
+   
+
+}
+
+Console.WriteLine("Press Enter to exit");
+Console.ReadLine();
